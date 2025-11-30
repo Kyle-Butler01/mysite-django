51c63d4e114fe6bdd8ccffe21219ac98283135d1
Kyle-Butler01 <gucigovislam39@gmail.com>
2025-11-30 23:04:08 +0300

    Необходимые настройки перед деплоем

--- webapp/settings.py
+++ webapp/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
