57553bc892bdf6dcc8e10e176da9fd888798b5c3
Kyle-Butler01 <gucigovislam39@gmail.com>
2025-12-01 22:58:26 +0300

    Еще небольшие изменения

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-DEBUG = os.getenv('DEBUG')
-
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = ['*']
